877607f9e9694b68141a3ae216f18e9ecbd1afcf
Syed Ali Mehdi <syedalimehdi59@gmail.com>
2023-10-11 14:40:29 +0500

    php8.1-installation.sh

---
+++ php8.1-installation.sh
@@ -0,0 +1,21 @@
+apt install curl sudo systemctl -y
+
+apt install software-properties-common -y
+
+sudo apt-add-repository ppa:ondrej/php -y
+
+sudo apt-get update
+
+sudo apt install --no-install-recommends php8.1 -y
+
+php -v
+
+sudo apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
+
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+
+HASH=`curl -sS https://composer.github.io/installer.sig`
+
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
